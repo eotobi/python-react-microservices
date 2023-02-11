067a44dab14f6c9e2f3c4ae84abbe6518a31922d
Elisha Otobi <eotobi@gmail.com>
2023-02-11 21:57:14 +0300

    new flask app: main and complete products and admin app

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,20 +1,46 @@
-from rest_framework import viewsets
+from random import random
 
-class ProductViewSet(viewsetsViewSet):
-	"""This view class implements the crud operations"""
-	def list(self, request): #api/products
-		pass
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-	def create(self, request): #api/products
-		pass
+from .models import Product, User
+from .serializers import ProductSerializer
 
-	def retrieve(self, request, pk=None): #api/products/<str:id>
-		pass
+class ProductViewSet(viewsets.ViewSet):
+	"""This class implements the crud operations"""
+	def list(self, request):
+		products = Product.objects.all()
+		serializer = ProductSerializer(products, many=True)
+		return Response(serializer.data)
 
-	def update(self, request, pk=None): #api/products/<str:id>
-		pass
+	def create(self, request):
+		serializer = ProductSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status= status.HTTP_201_CREATED)
 
-	def delete(self, request, pk=None): #api/products/<str:id>
-		pass
+	def retrieve(self, request, pk=None):
+		product = Product.objects.get(id=pk)
+		serializer = ProductSerializer(product)
+		return Response(serializer.data)
 
+	def update(self, request, pk=None):
+		product = Product.objects.get(id=pk)
+		serializer = ProductSerializer(instance=product, data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
+	def destroy(self, request, pk=None):
+		product = Product.objects.get(id=pk)
+		product.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserApiView(APIView):
+	def get(self, _):
+		users = User.objects.all()
+		user = random.choice(users)
+		return Response({
+			'id': user.id
+		})
